843df37eaea393df9d4593701e0d588482bc2793
Ariefsetiawan900 <arief.setiawan8788@gmail.com>
2020-04-01 17:59:59 +0700

    create Login & Register

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,21 +1,63 @@
 import React from "react"
-import { View, Text, StyleSheet, TextInput, Touchable } from "react-native"
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native"
+import * as firebase from 'firebase'
 
 export default class LoginScreen extends React.Component {
+    state = {
+        email: "",
+        password: "",
+        errorMessage: null
+    }
+
+    handleLogin = () => {
+        const {email, password } = this.state
+        
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(email, password)
+            .catch(error => this.setState({ errorMessage: error.message}))
+    }
+
+
+
     render() {
         return (
             <View style={styles.container}>
                <Text style={styles.greeting}>{`Hello again.\nWelcome back`}</Text>
+
             <View style={styles.errorMessage}>
-                <Text>Error</Text>
+               {this.state.errorMessage && <Text style={styles.error}>{this.state.errorMessage}</Text>}
             </View>
 
             <View style={styles.form}>
                 <View>
                     <Text style={styles.inputTitle}>Email Addres</Text>
-                    <TextInput style={styles.input} autoCapitalize="none"> </TextInput>
+                    <TextInput style={styles.input} 
+                    autoCapitalize="none"
+                    onChangeText={email => this.setState({ email })}
+                    value={this.state.email}
+                    ></TextInput>
+                </View>
+
+                <View style={{ marginTop: 32 }}>
+                    <Text style={styles.inputTitle}>Password</Text>
+                    <TextInput style={styles.input} 
+                    secureTextEntry 
+                    autoCapitalize="none"
+                    onChangeText={password => this.setState({ password })}
+                    value={this.state.password}
+                    ></TextInput>
                 </View>
             </View>
+            <TouchableOpacity style={styles.button} onPress={this.handleLogin}>
+                <Text style={{ color: "#FFF", fontWeight: "500"}}>Sign in</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={{ alignSelf: "center", marginTop: 32}} onPress={() => this.props.navigation.navigate("Register")}>
+                <Text style={{ color: "#414959", fontSize: 13}}>
+                        New to SocialApp? <Text style={{ fontWeight:"500", color: "#E9446A"}}>Sign Up</Text>
+                </Text>
+            </TouchableOpacity>
             </View>
         )
     }
@@ -24,7 +66,44 @@ export default class LoginScreen extends React.Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    greeting: {
+        marginTop: 32,
+        fontSize: 18,
+        fontWeight: "400",
+        textAlign: "center"
+    },
+    errorMessage: {
+        height: 72,
+        alignItems: "center",
         justifyContent: "center",
-        alignItems: "center"
+        marginHorizontal: 30
+    },
+    error: {
+        color: "#E9446A"
+    },
+    form : {
+        marginBottom: 48,
+        marginHorizontal: 30
+    },
+    inputTitle: {
+        color: "#8A8F9E",
+        fontSize: 10,
+        textTransform: "uppercase"
+    },
+    input: {
+        borderBottomColor: "#8A8F9E",
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        height: 40,
+        fontSize: 15,
+        color: "#161F3D"
+    },
+    button: {
+        marginHorizontal: 30,
+        backgroundColor : "#E9446A",
+        borderRadius: 4,
+        height: 52,
+        alignItems: "center",
+        justifyContent: "center"
     }
 })
